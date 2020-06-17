9338c7515610934ca383b7a9b7947f1e945d8123
Dongwan Kim <dongwan.don.kim@gmail.com>
2020-06-17 04:38:54 -0700

    api call prefix fixed

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,16 +1,13 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
-
 dotenv.config();
-
-const KEY = process.env.API_KEY;
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
     maxResults: 5,
-    key: KEY,
+    key: process.env.REACT_APP_YOUTUBE,
     type: 'video',
   },
 });
